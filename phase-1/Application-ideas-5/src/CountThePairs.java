94884e43a35634fb14e6bc51abe5787b851b88e0
saqib <saqibansari2011@gmail.com>
2026-02-15 10:41:35 +0530

    optimized

--- phase-1/Application-ideas-5/src/CountThePairs.java
+++ phase-1/Application-ideas-5/src/CountThePairs.java
@@ -1,3 +1,33 @@
+//import java.util.*;
+//
+//class CountThePairs {
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        int T = sc.nextInt();
+//
+//        while(T-- > 0){
+//            int n = sc.nextInt();
+//            int x = sc.nextInt();
+//
+//            int[] arr = new int[n];
+//            for(int i = 0; i < n; i++){
+//                arr[i] = sc.nextInt();
+//            }
+//
+//            long count = 0;
+//
+//            for(int i = 0; i < n; i++){
+//                for(int j = 0; j < n; j++){
+//                    if(i != j && arr[i] + arr[j] <= x){
+//                        count++;
+//                    }
+//                }
+//            }
+//
+//            System.out.println(count);
+//        }
+//    }
+//}
 import java.util.*;
 
 class CountThePairs {
@@ -14,17 +44,22 @@ class CountThePairs {
                 arr[i] = sc.nextInt();
             }
 
+            Arrays.sort(arr);
+
+            int left = 0;
+            int right = n - 1;
             long count = 0;
 
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    if(i != j && arr[i] + arr[j] <= x){
-                        count++;
-                    }
+            while(left < right){
+                if(arr[left] + arr[right] <= x){
+                    count += (right - left);
+                    left++;
+                } else {
+                    right--;
                 }
             }
 
-            System.out.println(count);
+            System.out.println(2L * count);
         }
     }
 }
